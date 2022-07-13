9974fdbe297dac65d9efdd7681e6f09422b2aa91
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-07-13 19:03:33 -0400

    styling and started auth

--- main_app/models.py
+++ main_app/models.py
@@ -16,7 +16,7 @@ class Cards(models.Model):
     move_name_2 = models.CharField(max_length=100)
     weakness_type = models.CharField(max_length=100)
     card_type = models.CharField(max_length=100)
-  
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
     def detail(self):
